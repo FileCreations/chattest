e5b82d4fd73ff0d3aa939933c0350482ea143f92
FileCreations <minorsarenotok1@gmail.com>
2024-08-19 23:41:26 -0500

    Update RunAsTrustedInstaller.ps1

--- RunAsTrustedInstaller.ps1
+++ RunAsTrustedInstaller.ps1
@@ -3,19 +3,20 @@ param (
 )
 
 try {
+    # Start the TrustedInstaller service (Windows Modules Installer) if it's not running
+    $serviceName = 'TrustedInstaller'
+    $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+
+    if ($service -and $service.Status -ne 'Running') {
+        Start-Service -Name $serviceName
+        Write-Host "Starting the TrustedInstaller service..."
+    }
+
     # Prompt the user for confirmation
     $confirmation = Read-Host "Are you sure you want to run $filePath as TrustedInstaller? (Y/N)"
     if ($confirmation -eq "Y") {
-        $serviceName = 'TrustedInstaller'
-        $service = Get-Service -Name $serviceName -ErrorAction Stop
-
-        # Start the TrustedInstaller service if it's not running
-        if ($service.Status -ne 'Running') {
-            Start-Service -Name $serviceName
-        }
-
         # Run the selected file as TrustedInstaller
-        Start-Process -FilePath "psexec.exe" -ArgumentList "-accepteula -s -i \"$filePath\"" -NoNewWindow -Wait
+        Start-Process -FilePath "psexec.exe" -ArgumentList "-accepteula -s -i `"$filePath`"" -NoNewWindow -Wait
     } else {
         Write-Host "Operation canceled."
     }
