2f97de8a559bc99d21310249bc58c119efc94a39
FileCreations <minorsarenotok1@gmail.com>
2024-08-20 07:17:41 -0500

    Update RunAsTrustedInstaller.ps1

--- RunAsTrustedInstaller.ps1
+++ RunAsTrustedInstaller.ps1
@@ -9,11 +9,20 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit
 }
 
+# Start the TrustedInstaller service if it's not running
+$serviceName = 'TrustedInstaller'
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+
+if ($service -and $service.Status -ne 'Running') {
+    Start-Service -Name $serviceName
+    Write-Host "Starting the TrustedInstaller service..."
+}
+
 # Confirm with the user
 $confirmation = Read-Host "Are you sure you want to run $filePath as TrustedInstaller? (Y/N)"
 if ($confirmation -eq "Y") {
-    # Run the file as TrustedInstaller using PsExec
-    Start-Process -FilePath "psexec.exe" -ArgumentList "-accepteula -s -i -u NT AUTHORITY\TrustedInstaller `"$filePath`"" -NoNewWindow -Wait
+    # Run the file as SYSTEM with the TrustedInstaller service context
+    Start-Process -FilePath "psexec.exe" -ArgumentList "-accepteula -s -i `"$filePath`"" -NoNewWindow -Wait
 } else {
     Write-Host "Operation canceled."
 }
